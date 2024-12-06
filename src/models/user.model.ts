654d857fe2c2a1312f489a4e4877a22de60c450a
muskan <muskanloach984@gmail.com>
2024-12-06 13:53:06 +0530

    user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,49 @@
+import mongoose, { Document } from "mongoose";
+
+// Linktree structure
+interface Linktree {
+  templateName: string;
+  links: mongoose.Types.ObjectId[]; // References to Link documents
+}
+
+// User interface, extending Mongoose's Document
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  avatar?: string; 
+  linktree?: Linktree; 
+}
+
+// User schema
+const userSchema = new mongoose.Schema<User>(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      default: "https://example.com/default-avatar.png",
+    },
+    linktree: {
+      type: {
+        templateName: { type: String, required: true },
+        links: [{ type: mongoose.Schema.Types.ObjectId, ref: "Link" }],
+      },
+      required: false, // Make the entire `linktree` object optional
+    },
+  },
+  { timestamps: true } // timestamps for createdAt and updatedAt
+);
+
+export default mongoose.model<User>("User", userSchema);
