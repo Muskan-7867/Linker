3c3a677d1d7da5bb51f9e3220b2638027c715148
muskan <muskanloach984@gmail.com>
2024-12-06 15:58:57 +0530

    linkcontroller

--- src/controller/linkController.ts
+++ src/controller/linkController.ts
@@ -1,37 +1,33 @@
 import { NextFunction, Request, Response } from "express";
-import linkModel from "../models/link.model";
+import linkModel from "../models/linktree.model";
 import createHttpError from "http-errors";
 
 // POST Controller: Create a new link
-export const createLink = async (req: Request,res: Response,next: NextFunction) => {
+export const createLink = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const { title, icon, url } = req.body;
 
-    // Validation
+    // Validate required fields
     if (!title || !icon || !url) {
       return next(createHttpError(400, "All fields are required"));
     }
 
     // Create a new link
-    const newLink = new linkModel({
-      title,
-      icon,
-      url,
-    });
+    const newLink = new linkModel({ title, icon, url });
 
-    // Save to the database
+    // Save the link to the database
     await newLink.save();
 
-    return res
-      .status(201)
-      .json({ message: "Link created successfully", link: newLink });
-  } catch {
+    res.status(201).json({ message: "Link created successfully", link: newLink });
+  } catch (error) {
+   
+    console.error(error);
     return next(createHttpError(500, "Internal Server Error"));
   }
 };
 
-// PUT Controller: Edit  link
-export const editLink = async (req: Request,res: Response,next: NextFunction) => {
+// PUT Controller: Edit an existing link
+export const editLink = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const { id } = req.params;
     const { title, icon, url } = req.body;
@@ -48,13 +44,16 @@ export const editLink = async (req: Request,res: Response,next: NextFunction) =>
       { new: true, runValidators: true }
     );
 
+    // If no link is found
     if (!updatedLink) {
       return next(createHttpError(404, "Link not found"));
     }
-return res
-      .status(200)
-      .json({ message: "Link updated successfully", link: updatedLink });
-  } catch {
+
+
+    res.status(200).json({ message: "Link updated successfully", link: updatedLink });
+  } catch (error) {
+   
+    console.error(error);
     return next(createHttpError(500, "Internal Server Error"));
   }
 };
