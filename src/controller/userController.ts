c366ea90ae104fa6db08234e4069365d68ba4764
muskan <muskanloach984@gmail.com>
2024-12-06 13:52:34 +0530

    add code for generate unique username

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -2,19 +2,20 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import userModel from "../models/userModel";
 
+import mongoose from "mongoose"; 
+import userModel from "../models/user.model";
 
 const demo = async (req: Request, res: Response) => {
-  res.send("demo succcess");
+  res.send("demo success");
 };
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { name, email, password } = req.body;
+    const { username, email, password } = req.body;
 
     // Validate request fields
-    if (!name || !email || !password) {
+    if (!username || !email || !password) {
       const error = createHttpError(400, "All fields are required");
       return next(error);
     }
@@ -27,12 +28,12 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     }
 
     // Hash the password
-    const saltRounds = 10; // Adjust salt rounds for desired security
+    const saltRounds = 10; 
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
     // Create new user
     const newUser = await userModel.create({
-      name,
+      username,
       email,
       password: hashedPassword,
     });
@@ -61,29 +62,41 @@ const loginUser = async (req: Request, res: Response, next: NextFunction) => {
     if (!email || !password) {
       throw createHttpError(400, "All fields are required");
     }
-  
+
     const user = await userModel.findOne({ email });
-  
+
     if (!user) {
       throw createHttpError(404, "User not found");
     }
 
-    const isPasswordMatch = await bcrypt.compare(password, user.password)
-    if(!isPasswordMatch){
-      return next(createHttpError(404,"Password is incorrect"))
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      throw createHttpError(401, "Password is incorrect");
     }
 
-    //create accesstoken
+    // Check if username exists, and if not, create a unique username
+    if (!user.username) {
+      const userId = user._id as mongoose.Types.ObjectId; // Type assertion to ObjectId
+      const uniqueUsername = `${user.username.replace(/\s+/g, '').toLowerCase()}_${userId.toString().slice(-4)}`;
+      user.username = uniqueUsername;
+      await user.save();
+    }
+
+    // Create access token
     const token = jwt.sign(
-      { userId: user._id, email: user.email },
+      { userId: user._id, email: user.email, username: user.username },
       process.env.JWT_SECRET as string,
       { expiresIn: "1h" }
     );
-  
-    res.json({ accesstoken: token });
+
+    res.json({
+      message: "Login successful",
+      accesstoken: token,
+      username: user.username,
+    });
   } catch (error) {
-    next(error); 
+    next(error);
   }
-}  
+};
 
 export { demo, createUser, loginUser };
