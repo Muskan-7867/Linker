dc769dc7b111524cba496b14df5f765aa7bc4454
muskan <muskanloach984@gmail.com>
2024-12-06 15:59:11 +0530

    add routes

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,23 @@
 import express from "express";
+
+import userRouter from "./router/userRouter"; 
+
+
 const app = express();
 
-//routers
-import userRouter from './router/userRouter'
-
-
-//middleware
+// Middleware
 app.use(express.json());
 
+// Welcome Route
 app.get("/", (req, res) => {
-  res.json({ message: "welcome to link tree" });
+  res.json({ message: "Welcome to Link Tree" });
 });
 
-//router
-
-
+// Routes
 app.use("/api/v1/user", userRouter);
+
+// Links Routes
+app.post("/api/v1/links", userRouter); 
+app.put("/api/v1/links/:id", userRouter); 
 
 export default app;
