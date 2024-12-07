8a9aa0d47f37badedd3f809e791c0f89ef2fecca
muskan <muskanloach984@gmail.com>
2024-12-07 23:31:23 +0530

    add delete linktree code

--- src/controller/linkController.ts
+++ src/controller/linkController.ts
@@ -3,10 +3,8 @@ import linkModel from "../models/linktree.model";
 import createHttpError from "http-errors";
 import linktreeModel from "../models/linktree.model";
 
-
-
 // POST Controller: Create a new link
-export const createLink = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+export const createLinktree = async (req: Request,res: Response,next: NextFunction): Promise<void> => {
   try {
     const { treeName, title, icon, url } = req.body;
 
@@ -24,26 +22,33 @@ export const createLink = async (req: Request, res: Response, next: NextFunction
     // Save the document to the database
     await newLink.save();
 
-    res.status(201).json({ message: "Link created successfully", link: newLink });
+    res
+      .status(201)
+      .json({ message: "Link created successfully", link: newLink });
   } catch (error) {
     console.error(error);
     return next(createHttpError(500, "Internal Server Error"));
   }
 };
-export const editLink = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+export const editLinktree = async (req: Request,res: Response,next: NextFunction): Promise<void> => {
   try {
     const { id, treeName, links } = req.body;
 
     // Validate required fields
     if (!id || !treeName || !Array.isArray(links)) {
-      return next(createHttpError(400, "'id', 'treeName', and 'links' fields are required"));
+      return next(
+        createHttpError(
+          400,
+          "'id', 'treeName', and 'links' fields are required"
+        )
+      );
     }
 
     //  Add default value  if icon is required for each link
-    links.forEach(link => {
+    links.forEach((link) => {
       if (!link.icon) {
         //  default icon
-        link.icon = "defaultIcon.svg";  
+        link.icon = "defaultIcon.svg";
       }
     });
 
@@ -68,8 +73,24 @@ export const editLink = async (req: Request, res: Response, next: NextFunction):
     next(createHttpError(500, "Internal Server Error"));
   }
 };
+export const deleteLinktree = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return next(createHttpError(400, "id is required"));
+    }
 
+    const deletedLinktree = await linktreeModel.findByIdAndDelete(id);
+    if (!deletedLinktree) {
+      return next(createHttpError(404, "Linktree not found."));
+    }
 
-
-
-
+    // Send a success response
+    res.status(200).json({
+      message: "Linktree deleted successfully.",
+      linktree: deletedLinktree,
+    });
+  } catch {
+    return next(createHttpError(500, "Internal Server Error"));
+  }
+};
