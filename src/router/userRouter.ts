d978e6d0426502a60f9a27e01061445ad1d4c46d
muskan <muskanloach984@gmail.com>
2024-12-07 23:01:16 +0530

    user router

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 import { createUser, loginUser, demo } from "../controller/userController";
-import { createLink, editLink } from "../controller/linkController";
+
 
 // Demo route
 router.route('/demo').get(demo);
@@ -9,9 +9,5 @@ router.route('/demo').get(demo);
 // User registration and login routes
 router.route('/register').post(createUser);
 router.route('/login').post(loginUser);
-
-// Link management routes
-router.route('/links').post(createLink); // Create a link
-router.route('/links/:id').put(editLink); // Edit an existing link
 
 export default router;
