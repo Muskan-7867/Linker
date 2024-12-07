1765bf551843228db976a1ea23054da81fcab612
muskan <muskanloach984@gmail.com>
2024-12-07 21:42:40 +0530

    linktree router

---
+++ src/router/linktree.router.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const router = express.Router();
+import { createLink, editLink } from "../controller/linkController";
+
+
+// Link management routes
+
+router.route('/create').post(createLink); // Create a link
+router.route('/update/:p').put(editLink); // Edit an existing link
+
+export default router;
