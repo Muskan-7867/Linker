2cf2aaa7adc57bd386a03cc4edb5a7e718892b56
muskan <muskanloach984@gmail.com>
2024-12-09 22:40:27 +0530

    new package add

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+      }
+
+    },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-motion')], 
 }
