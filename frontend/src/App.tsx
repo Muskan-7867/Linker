d6b8b39a7789d6768d3829c779f8351053630aaa
muskan <muskanloach984@gmail.com>
2024-12-20 23:51:46 +0530

    add provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,54 @@
-import { BrowserRouter as Router, Routes, Route,useLocation  } from "react-router-dom";
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { LinksProvider } from './components/LinksContext';
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Templates from "./components/Template/Template";
 import TemplateDetail from "./components/Template/TemplateDetail";
+import LinksDisplay from './components/LinkDisplay';
+import LinkForm from './components/Form';
 
-const App: React.FC =() =>  {
+const App: React.FC = () => {
   const location = useLocation();
-
-  // Condition to check if the current path is a template detail page
   const hideNavbar = location.pathname.startsWith('/templates/') && location.pathname !== '/templates';
+
   return (
     <>
       <div>
-      {!hideNavbar && <Navbar />}
-    
+        {!hideNavbar && <Navbar />}
         <Routes>
-        <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/templates" element={<Templates />} />
           <Route path="/templates/:id" element={<TemplateDetail />} />
+          <Route path="/links" element={
+            <div>
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <h2>Links</h2>
+                <button>Add Link</button>
+              </div>
+              <LinksDisplay />
+              <LinkForm />
+            </div>
+          } />
         </Routes>
       </div>
     </>
   );
 }
+
 const AppWrapper: React.FC = () => {
   return (
-    <Router>
-      <App />
-    </Router>
+    <LinksProvider>
+      <Router>
+        <App />
+      </Router>
+    </LinksProvider>
   );
 };
 
 export default AppWrapper;
-
-
