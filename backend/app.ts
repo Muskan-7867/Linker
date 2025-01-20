c9b788a183660e2e001498885480b5947f979308
muskan <muskanloach984@gmail.com>
2025-01-20 23:42:36 +0530

    add cors

--- backend/app.ts
+++ backend/app.ts
@@ -1,12 +1,21 @@
 import express from "express";
-import  userRouter from "../backend/src/router/userRouter"
-import  linkTreeRouter from "../backend/src/router/linktree.router"
+import userRouter from "../backend/src/router/userRouter";
+import linkTreeRouter from "../backend/src/router/linktree.router";
+import cors from 'cors'
 
 const app = express();
+
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
 
 // Welcome Route
 app.get("/", (req, res) => {
@@ -16,6 +25,6 @@ app.get("/", (req, res) => {
 // Routes
 app.use("/api/v1/user", userRouter);
 // Links Routes
-app.use("/api/v1/link", linkTreeRouter); 
+app.use("/api/v1/link", linkTreeRouter);
 
 export default app;
