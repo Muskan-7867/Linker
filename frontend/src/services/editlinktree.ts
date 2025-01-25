fef4ee19949ec93bd7e3ac3d076dd12ec6c7b316
muskan <muskanloach984@gmail.com>
2025-01-26 00:07:06 +0530

    update code

--- frontend/src/services/editlinktree.ts
+++ frontend/src/services/editlinktree.ts
@@ -1,31 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
-
-const API_URL = "http://localhost:8000/api/v1/link";
-
-// Function to handle editing a Linktree
-export const editLinktree = async (id: string, treeName: string, links: { title: string; icon: string; link: string }[]) => {
+// Function to update Linktree
+export const editLinktree = async (
+  id: string,
+  treeName: string,
+  links: { title: string; icon: string; link: string }[]
+) => {
   try {
-    const response = await axios.put(
-      `${API_URL}/update`, 
-      { treeName, links }
-    );
-    return response.data;
+    const response = await axios.put(`http://localhost:8000/api/vi/link/update${id}`, {
+      treeName,
+      links,
+    });
+    return response.data; 
   } catch (error) {
     console.error("Error updating Linktree:", error);
-    
-  }
-};
-
-// Function to handle deleting a Linktree
-export const deleteLinktree = async (id: string) => {
-  try {
-    const response = await axios.delete(
-      `${API_URL}/delete/${id}` 
-    );
-    return response.data;
-  } catch (error) {
-    console.error("Error deleting Linktree:", error);
-   
+    throw error; 
   }
 };
