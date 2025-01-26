38d4dbe09d49602a71651a70adf35ee505b8282d
muskan <muskanloach984@gmail.com>
2025-01-26 22:24:19 +0530

    update route

--- backend/src/router/linktree.router.ts
+++ backend/src/router/linktree.router.ts
@@ -6,7 +6,8 @@ import {  createLinktree,  deleteLinktree,  editLinktree } from "../controller/l
 // Link management routes
 
 router.route('/create').post(createLinktree); // Create a linktree
-router.route('/update').put(editLinktree); // Edit an existing linktree
+router.route('/edit').put(editLinktree);
+
 router.route('/delete/:id').delete(deleteLinktree);
 
 export default router;
