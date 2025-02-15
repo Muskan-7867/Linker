a56d82fcab60aac9583b91d190622e4e56e118b9
muskan <muskanloach984@gmail.com>
2025-02-15 16:13:38 +0530

    edit linktree

--- frontend/src/services/editlinktree.ts
+++ frontend/src/services/editlinktree.ts
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-export const editLinktree = async (data: { id: string; treeName: string; links: { title: string; link: string; icon?: string }[] }) => {
+export const editLinktree = async (data: { id: string; treeName: string; links: { title: string; url: string; icon?: string }[] }) => {
   try {
     const response = await axios.put("http://localhost:8000/api/v1/link/edit", data, {
       headers: { "Content-Type": "application/json" }, 
